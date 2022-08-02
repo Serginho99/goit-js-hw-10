5a956f5e4dbb3da678f963aefeec7e0897043d32
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-08-02 15:22:50 +0300

    update

--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -3,6 +3,9 @@ const FILTER_RESPONSE = `name,capital,flags,languages,population`;
 export function fetchCountries(name) {
   return fetch(`${BASE_URL}/name/${name}?fields=${FILTER_RESPONSE}`).then(
     resolve => {
+      if (!resolve.ok) {
+        throw new Error(resolve.status);
+      }
       return resolve.json();
     }
   );
